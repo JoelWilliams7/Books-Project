2dcf269e28264db57a8b98be42abb79aef96ff3f
Joel Williams <109452946+JoelWilliams7@users.noreply.github.com>
2023-03-20 11:53:01 -0400

    added supabase db

--- src/Context/books.js
+++ src/Context/books.js
@@ -1,5 +1,12 @@
 import { createContext, useState, useCallback } from "react";
-import axios from "axios";
+
+import { createClient } from "@supabase/supabase-js";
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_PUBLIC_KEY
+);
 
 const BooksContext = createContext({});
 
@@ -7,44 +14,38 @@ function Provider({ children }) {
   const [books, setBooks] = useState([]);
 
   const fetchBooks = useCallback(async () => {
-    const response = await axios.get("http://localhost:3001/books");
-
-    setBooks(response.data);
+    const { error, data } = await supabase.from("books").select("title, id");
+    if (error) {
+      console.log(error);
+      return;
+    }
+    setBooks(data);
   }, []);
 
   const editBookById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle,
-    });
+    const response = await supabase
+      .from("books")
+      .update({ title: newTitle })
+      .eq("id", id);
 
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, ...response.data };
-      }
-
-      return book;
-    });
-
-    setBooks(updatedBooks);
+    if (response.status === 204) {
+      await fetchBooks();
+    }
   };
 
   const deleteBookById = async (id) => {
-    await axios.delete(`http://localhost:3001/books/${id}`);
+    const response = await supabase.from("books").delete().eq("id", id);
 
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id;
-    });
-
-    setBooks(updatedBooks);
+    if (response.status === 204) {
+      await fetchBooks();
+    }
   };
 
-  const createBook = async (title) => {
-    const response = await axios.post("http://localhost:3001/books", {
-      title,
-    });
-
-    const updatedBooks = [...books, response.data];
-    setBooks(updatedBooks);
+  const createBook = async (newTitle) => {
+    const response = await supabase.from("books").insert({ title: newTitle });
+    if (response.status === 201) {
+      await fetchBooks();
+    }
   };
 
   const valueToShare = {
